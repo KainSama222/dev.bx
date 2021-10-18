123e1646913b195c5b3580db69042bb9cbf01c01
MalozenovIgor <kainsamaelis@gmail.com>
2021-10-18 15:23:02 +0200

    Homework-2: выполнил домашнее задание 2.

--- index.php
+++ index.php
@@ -1,37 +1,13 @@
 <?php
 
-$socket = fsockopen("test.shelenkov.com", 80, $errorCode, $errorString);
-if (!$socket)
-{
-	echo "$errorCode ($errorString)<br />\n";
-	die();
+include '..\dev.bx\movies.php';
+
+$age = readline("Введи свой возраст:");
+if (is_numeric($age)) {
+	printMovies($movies, $age);
+} else {
+	echo "Вы ввели не правильное значение.";
 }
-$content  = "--part" . "\r\n";
-$content .= "Content-Disposition: form-data; name=\"field1\"" . "\r\n";
-$content .= "\r\n";
-$content .= "value1" . "\r\n";
-
-$content .= "--part--";
-
-
-$result  = "POST /up/post.php HTTP/1.1" . "\r\n";
-$result .= "Host: test.shelenkov.com" . "\r\n";
-$result .= "Content-Type: multipart/form-data; boundary=\"part\"" . "\r\n";
-$result .= "Content-Length: ".strlen($content) . "\r\n";
-$result .= "Connection: close" . "\r\n";
-$result .= "\r\n";
-$result .= $content;
-
-
-fwrite($socket, $result);
-while (!feof($socket))
-{
-	echo fgets($socket);
-}
-fclose($socket);
-
-
-
 
 
 
